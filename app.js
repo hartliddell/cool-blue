b622419b53a737dde0a4c6248770ec9ed5a9e5bb
Hart Liddell <hart.liddell@dispel.io>
2021-03-12 14:32:38 -0600

    Update sentry

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const app = express();
 
 Sentry.init({
   dsn: "https://6677d262ab114bf5a1c5bf7a35f7bcd7@o550327.ingest.sentry.io/5673800",
-  integrations: [
-    // enable HTTP calls tracing
-    new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
-    new Tracing.Integrations.Express({ app }),
-  ],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
@@ -19,27 +13,15 @@ Sentry.init({
   tracesSampleRate: 1.0,
 });
 
-// RequestHandler creates a separate execution context using domains, so that every
-// transaction/span/breadcrumb is attached to its own Hub instance
-app.use(Sentry.Handlers.requestHandler());
-// TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler());
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/error', () => {
   throw new Error('oh no. an error!');
-});
-
-// The error handler must be before any other error middleware and after all controllers
-app.use(Sentry.Handlers.errorHandler());
-
-// Optional fallthrough error handler
-app.use(function onError(err, req, res) {
-  // The error id is attached to `res.sentry` to be returned
-  // and optionally displayed to the user for support.
-  res.statusCode = 500;
-  res.end(res.sentry + "\n");
 });
 
 const port = process.env.PORT || 3000;
